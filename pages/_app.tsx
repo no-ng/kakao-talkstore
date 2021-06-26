745313ee907db39423079f74f4eb9d46291754e4
johnny-mh <romz1212@gmail.com>
2021-06-26 16:56:09 +0900

    chore: eslint, prettier, husky 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
 
