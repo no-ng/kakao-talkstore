9cc215ec39c83a5f3485c3e2c5694ea9e9e00bd9
johnny-mh <romz1212@gmail.com>
2021-07-06 22:34:30 +0900

    refactor: 메인탭 grid로 변경

--- components/layout/MainTab.style.ts
+++ components/layout/MainTab.style.ts
@@ -1,61 +1,53 @@
 import styled from 'styled-components';
-import { headerHeight, screenSmall } from '../../styles/util';
+import { headerHeight } from '../../styles/util';
 
 const _MainTab = styled.div`
-  position: absolute;
-  top: ${headerHeight}px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
-  background-color: #fff;
   width: 100%;
+  height: ${headerHeight}px;
 
   ul {
-    top: 0px;
-    display: table;
-    table-layout: fixed;
+    height: ${headerHeight}px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
     text-align: center;
-    width: 100%;
-    min-width: 320px;
-    max-width: 750px;
-    margin: auto;
 
     li {
-      display: table-cell;
       white-space: nowrap;
-    }
 
-    a {
-      height: ${headerHeight}px;
-      line-height: ${headerHeight}px;
-      font-size: 16px;
-      letter-spacing: -0.5px;
-      text-align: center;
-
-      span {
-        display: inline-block;
-        position: relative;
-        vertical-align: top;
-        white-space: nowrap;
+      a {
         line-height: ${headerHeight}px;
-        color: #000;
-      }
+        height: ${headerHeight}px;
+        line-height: ${headerHeight}px;
+        font-size: 16px;
+        letter-spacing: -0.5px;
+        text-align: center;
 
-      &[aria-selected='true'] {
-        font-weight: 500;
+        span {
+          display: inline-block;
+          position: relative;
+          vertical-align: top;
+          white-space: nowrap;
+          line-height: ${headerHeight}px;
+          color: #000;
+        }
 
-        span:after {
-          left: -4px;
-          right: -4px;
-          position: absolute;
-          bottom: -1px;
-          height: 2px;
-          background-color: #4a4a4a;
-          content: '';
+        &[aria-selected='true'] {
+          font-weight: 500;
+
+          span:after {
+            left: -4px;
+            right: -4px;
+            position: absolute;
+            bottom: -1px;
+            height: 2px;
+            background-color: #4a4a4a;
+            content: '';
+          }
         }
       }
     }
   }
-
-  ${screenSmall}
 `;
 
 export default _MainTab;
