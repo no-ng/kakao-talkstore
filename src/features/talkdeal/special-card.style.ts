c097f3ddb6f4625d22c5806607c644f3b8ede082
johnny-mh <romz1212@gmail.com>
2021-07-17 00:44:30 +0900

    refactor: 스페셜카드 개발되어따!

--- src/features/talkdeal/special-card.style.ts
+++ src/features/talkdeal/special-card.style.ts
@@ -4,11 +4,12 @@ const _SpecialCard = styled.div<any>`
   aspect-ratio: 1/1;
   margin: 0 18px 40px;
   position: relative;
+  border-radius: 8px;
+  overflow: hidden;
 
   img {
     display: block;
     width: 100%;
-    border-radius: 8px;
   }
 
   .top {
@@ -16,7 +17,6 @@ const _SpecialCard = styled.div<any>`
     top: 10px;
     left: 10px;
     font-size: 0.9rem;
-    font-weight: 700;
     color: black;
     letter-spacing: 0.05rem;
 
@@ -26,10 +26,73 @@ const _SpecialCard = styled.div<any>`
       line-height: 24px;
       border-radius: 3px;
       padding: 0 6px 0 8px;
+      font-weight: 700;
       mask-image: radial-gradient(circle at center left, transparent 3px, black 1px);
 
       span {
-        color: ${(props) => props.specialCard.specialCardCoupon.backgroundColor};
+        color: ${(props) => props.specialCard?.specialCardCoupon?.backgroundColor};
+      }
+    }
+
+    .timer {
+      display: inline-block;
+      line-height: 24px;
+      color: #fff;
+      margin-left: 4px;
+      padding: 0 7px;
+      border-radius: 3px;
+      background-color: rgba(0, 0, 0, 0.65);
+
+      &.impended {
+        background-color: #ff5451;
+      }
+    }
+  }
+
+  .down {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 132px;
+    padding: 0 10px;
+    text-align: center;
+    background-color: ${(props) => props.specialCard?.specialCardBackgroundColor};
+
+    .title {
+      color: ${(props) => props.specialCard?.specialCardPrimaryColor};
+      display: block;
+      padding-top: 19px;
+      font-size: 1.2rem;
+      letter-spacing: -0.03rem;
+    }
+
+    .desc {
+      display: block;
+      padding-top: 5px;
+      font-size: 1.5rem;
+      line-height: 23px;
+      letter-spacing: -0.03rem;
+      color: ${(props) => props.specialCard.specialCardSecondaryColor};
+    }
+
+    .price {
+      display: block;
+      padding-top: 6px;
+      line-height: 19px;
+      color: #fff;
+
+      .special {
+        display: inline-block;
+        font-size: 1.2rem;
+        letter-spacing: -0.02rem;
+      }
+
+      .regular {
+        display: inline-block;
+        font-size: 1.1rem;
+        opacity: 0.5;
+        margin-left: 5px;
       }
     }
   }
--- src/features/talkdeal/special-card.tsx
+++ src/features/talkdeal/special-card.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+import { Duration } from 'luxon';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import Timer from '../../components/timer';
@@ -7,12 +9,17 @@ import { selectSpecialCard } from './talkdeal.slice';
 const SpecialCard = () => {
   const specialCard = useSelector(selectSpecialCard);
 
-  if (!specialCard) {
+  if (!specialCard || specialCard?.remainSeconds <= 0) {
     return <div />;
   }
 
-  const coupon = specialCard?.specialCardCoupon;
   const nf = new Intl.NumberFormat();
+
+  const coupon = specialCard?.specialCardCoupon;
+
+  const remainSec = Duration.fromObject({ seconds: specialCard.remainSeconds });
+  const showTimer = remainSec.days < 1;
+  const impended = remainSec.hours < 2;
 
   return (
     <_SpecialCard specialCard={specialCard}>
@@ -20,21 +27,43 @@ const SpecialCard = () => {
         <a>
           <img src={specialCard?.firstImageUrl} />
           <span className="top">
-            {coupon && (
+            {coupon?.value && (
               <span className="badge">
-                쿠폰<span>{coupon.value}%</span>
+                쿠폰
+                {coupon?.unit === 'RATE' ? (
+                  <span>{coupon.value}%</span>
+                ) : (
+                  <>
+                    <span>{nf.format(coupon.value)}</span>원
+                  </>
+                )}
               </span>
             )}
-            <span className="timer">
-              <Timer remainSeconds={specialCard.remainSeconds} opener={'talkdeal'} />
-            </span>
+            {showTimer && (
+              <motion.span
+                className={`timer ${impended && 'impended'}`}
+                animate={{
+                  y: [0, -2, 0],
+                }}
+                transition={{
+                  times: [0, 0.8, 1],
+                  duration: 0.5,
+                  repeat: Infinity,
+                  repeatDelay: 5,
+                  ease: 'circIn',
+                }}
+              >
+                <Timer remainSeconds={specialCard.remainSeconds} opener={'talkdeal'} />{' '}
+                남음
+              </motion.span>
+            )}
           </span>
           <span className="down">
             <span className="title">{specialCard.specialCardTitle}</span>
             <strong
               className="desc"
               dangerouslySetInnerHTML={{
-                __html: specialCard.specialCardDescription.replace(/\\n/g, '<br />'),
+                __html: specialCard.specialCardDescription.replace(/\n/g, '<br />'),
               }}
             ></strong>
             <span className="price">
