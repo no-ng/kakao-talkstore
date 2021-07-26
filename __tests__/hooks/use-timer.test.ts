f48176bf72e3e0c87fb3c70e396ae55faac0a1e5
johnny-mh <romz1212@gmail.com>
2021-07-27 01:43:43 +0900

    refactor: use-timer 코드 정리
    useEffect한번만 사용하도록 수정

--- __tests__/hooks/use-timer.test.ts
+++ __tests__/hooks/use-timer.test.ts
@@ -1,18 +1,34 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import useTimer from '../../src/hooks/use-timer';
 
 describe('hooks', () => {
   describe('use-timer', () => {
-    test('인자로 전달된 초가 하루 이상이면 일자를 표기한다', () => {
+    test('24시간 3분 초과 입력 시 남은 일자 표기', () => {
       const { result } = renderHook(() => useTimer(60 * 60 * 24 * 3));
 
       expect(result.current.day).toBe('3');
     });
 
-    test('인자로 전달된 초가 24시간 이내일 경우 시간을 표기한다', () => {
+    test('23시간 입력 시 23:00:00 표기', () => {
       const { result } = renderHook(() => useTimer(60 * 60 * 23));
 
       expect(result.current.time).toBe('23:00:00');
+    });
+
+    test('3분 뒤 당일이면 시간에 맞춰 타이머 표기', () => {
+      jest.useFakeTimers();
+
+      const { result } = renderHook(() => useTimer(60 * 60 * 24 + 3));
+
+      expect(result.current.day).toBe('1');
+
+      act(() => {
+        jest.advanceTimersByTime(5000);
+      });
+
+      expect(result.current.time).toBe('23:59:58');
+
+      jest.useRealTimers();
     });
   });
 });
--- src/hooks/use-timer.ts
+++ src/hooks/use-timer.ts
@@ -4,44 +4,47 @@ import { useEffect, useState } from 'react';
 const useTimer = (seconds: number): { day: string; time: string } => {
   const [day, setDay] = useState('');
   const [time, setTime] = useState('');
-  let duration = Duration.fromObject({ seconds });
 
-  const startTimer = () => {
-    setTime(duration.toFormat('hh:mm:ss'));
+  useEffect(() => {
+    let duration = Duration.fromObject({ seconds });
 
-    let timer = 0;
-
-    const tick = () => {
-      duration = duration.minus(1000);
+    const startTimer = () => {
       setTime(duration.toFormat('hh:mm:ss'));
-    };
 
-    timer = window.setInterval(() => tick(), 1000);
-
-    return () => clearInterval(timer);
-  };
-
-  if (duration.as('days') >= 1) {
-    const untilToday = Duration.fromObject({ day: 1, minutes: 3 }).minus(duration);
-
-    useEffect(() => setDay(`${Math.floor(duration.as('days'))}`), []);
-
-    // 3분뒤 당일이 되면 타이머를 시작한다
-    if (untilToday.as('minutes') <= 3) {
       let timer = 0;
 
-      useEffect(() => {
-        timer = window.setTimeout(() => {
-          setDay('');
-          startTimer();
-        }, untilToday.as('milliseconds'));
+      const tick = () => {
+        duration = duration.minus(1000);
+        setTime(duration.toFormat('hh:mm:ss'));
+      };
 
-        return () => clearTimeout(timer);
-      }, []);
+      timer = window.setInterval(() => tick(), 1000);
+
+      return () => clearInterval(timer);
+    };
+
+    if (duration.as('days') >= 1) {
+      setDay(`${Math.floor(duration.as('days'))}`);
+
+      // 3분 뒤 당일이 되면 타이머를 설정한다
+      const untilToday = duration.minus({ days: 1 });
+
+      if (untilToday.as('minutes') <= 3) {
+        let teardown: () => void;
+
+        window.setTimeout(() => {
+          duration = Duration.fromObject({ days: 1 });
+          teardown = startTimer();
+        }, untilToday.toMillis());
+
+        return () => teardown && teardown();
+      }
+
+      return;
     }
-  } else {
-    useEffect(() => startTimer(), []);
-  }
+
+    return startTimer();
+  }, []);
 
   return { day, time };
 };
